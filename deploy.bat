09ec746aa895078990d3b469f776a3abe249b4a9
shaharlitvin2000 <shahar070510@gmail.com>
2026-04-24 23:19:44 +0300

    Update

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,7 @@
 @echo off
+copy "C:\Users\blitv\icy-tower\desktop-link\index.html" "C:\Users\blitv\icy-tower\index.html" /Y
 cd C:\Users\blitv\icy-tower
-git add index.html
+git add -A
 git commit -m "Update"
 git push
 firebase deploy
