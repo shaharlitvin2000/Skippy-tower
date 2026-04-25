2a2a1fde5b3734a8a9803911b97ebc9c5744d59c
shaharlitvin2000 <shahar070510@gmail.com>
2026-04-25 17:07:02 +0300

    Update

--- deploy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-copy "C:\Users\blitv\Desktop\index.html" "C:\Users\blitv\icy-tower\index.html" /Y
-cd C:\Users\blitv\icy-tower
-git add -A
-git commit -m "Update"
-git push
-firebase deploy
-echo Done!
-pause
